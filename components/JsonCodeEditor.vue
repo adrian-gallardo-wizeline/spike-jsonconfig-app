ad2dd09a17f5602bb5e85e79936f03c6af0405b2
Adrian Gallardo <adrian.gallardo@wizeline.com>
2018-06-11 10:19:57 -0500

    Missing files

---
+++ components/JsonCodeEditor.vue
@@ -0,0 +1,41 @@
+<template>
+  <no-ssr class="json-code-editor">
+    <codemirror 
+      v-model="value"
+      :options="options"
+      @change="onChange"
+      ref="myEditor"
+    />
+  </no-ssr>
+</template>
+
+<script>
+const components = {}
+
+if (process.browser) {
+  components.codemirror = require('vue-codemirror-lite').codemirror
+
+  require('codemirror/mode/javascript/javascript')
+
+  require('codemirror/addon/hint/show-hint.js')
+  require('codemirror/addon/hint/show-hint.css')
+  require('codemirror/addon/hint/javascript-hint.js')
+
+  require('codemirror/addon/lint/json-lint.js')
+}
+
+export default {
+  props: ['value', 'options', 'onChange'],
+  components,
+}
+</script>
+
+<style scoped lang="scss">
+
+.json-code-editor,
+.json-code-editor /deep/ .vue-codemirror-wrap,
+.json-code-editor /deep/ .CodeMirror {
+  height: 100%;
+}
+
+</style>
